5bd486bbc3d2435f76f1058d599af720a83cfda6
Christian Lopez <christianlopez@christianlopezmac.local>
2023-09-16 13:10:23 -0400

    added basic parallax functions

--- citizenship-for-dummies/src/App.js
+++ citizenship-for-dummies/src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useRef } from 'react';
+import BackgroundExample from './images/exampleNatureImage.jpg';
+import ExampleGif from './images/exampleGif.gif'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import './App.css';
 
 function App() {
+  
+const ref = useRef();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <Parallax pages={4} ref={ref}>
+
+          <ParallaxLayer>
+            <h2>Welcome to my website</h2>
+          </ParallaxLayer>
+
+          <ParallaxLayer 
+          offset={1} 
+          speed={.5}>
+            <h2> Mystery Dude </h2>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+          offset={2}
+          speed={2}
+          factor={1}
+          style={{
+            backgroundImage: `url(${BackgroundExample})`,
+            backgroundSize: 'cover',
+          }}/>
+
+          <ParallaxLayer
+          sticky={{start:0, end:1.5}}
+          onClick={()=>ref.current.scrollTo(3)}>
+            <img src={ExampleGif}/>
+          </ParallaxLayer>
+
+        </Parallax>
     </div>
   );
 }
